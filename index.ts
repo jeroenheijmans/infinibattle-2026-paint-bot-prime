3778927e3bc9dd7b50e25d419f992c49ac94eb4c
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-03-15 22:54:37 +0100

    Re-simplify setup before starting prompt

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { createBanter } from "./chat";
 import { type ICommand } from "./commands";
-import type { TankDetails } from "./helpers";
 import type { EnvironmentMessage, StepState } from "./messages";
 import { executeStrategyForStep } from "./strategy";
 
@@ -9,37 +8,7 @@ const consoleIterator = console[Symbol.asyncIterator]();
 console.log("bot-start"); // mandatory!
 
 const environment = await readMessage<EnvironmentMessage>("environment"); // mandatory!
-const allObservedTankScanEvents: Record<number, TankDetails[]> = {};
 
-function recordTankStates(state: StepState) {
-  // Record my own state
-  allObservedTankScanEvents[state.Tank.Id] = allObservedTankScanEvents[state.Tank.Id] || [];
-  allObservedTankScanEvents[state.Tank.Id]?.push({
-    Step: state.Step,
-    TankId: state.Tank.Id,
-    Location: state.Tank.Location,
-    TurretHeading: state.Tank.TurretHeading,
-    Heading: state.Tank.Heading,
-    Health: state.Tank.Health,
-    IsEnemy: false,
-  });
-
-  // Record scanned states, friends and enemies
-  state.TankScans.forEach(s => {
-    allObservedTankScanEvents[s.TankId] = allObservedTankScanEvents[s.TankId] || [];
-    allObservedTankScanEvents[s.TankId]?.push({
-      Step: state.Step,
-      TankId: s.TankId,
-      Location: s.Location,
-      TurretHeading: s.TurretHeading,
-      Heading: s.Heading,
-      Health: s.Health,
-      IsEnemy: s.IsEnemy,
-    });
-  });
-}
-
-// process killed when runner feels it necessary
 while (true) {
   const state = await readMessage<StepState>("state"); // mandatory!
 
@@ -51,31 +20,8 @@ while (true) {
     t.Heading = normalize(t.Heading);
     t.TurretHeading = normalize(t.TurretHeading);
   });
-
-  recordTankStates(state);
   
-  const commands: ICommand[] = [executeStrategyForStep(environment, state, allObservedTankScanEvents)];
-
-  // if (state.Tank.Id === 1 && state.Step % 25 === 0) {
-  //   commands.push(new LogCommand(
-  //     "Observations: " +
-  //     Object.keys(allObservedTankScanEvents).map(k => allObservedTankScanEvents[k]?.length).join(", ")
-  //   ))
-  // }
-
-  // if (state.Tank.Id === 0) {
-  //   commands.push(
-  //       new LogCommand(``
-  //       + ` | Health = ${String(state.Tank.Health.Value).padStart(2)}`
-  //       + ` | Gun = ${String(state.Tank.GunEnergy.Value).padStart(2)}`
-  //       + ` | Location = ${String(Math.round(state.Tank.Location.X)).padStart(3)}, ${String(Math.round(state.Tank.Location.Y)).padStart(3)}`
-  //       + ` | Velocity = ${state.Tank.Velocity}`
-  //       + ` | Heading = ${String(state.Tank.Heading).padStart(3)}`
-  //       + ` | TurretHeading = ${String(state.Tank.TurretHeading).padStart(3)}`
-  //       + ` | Chat = ${String(state.Tank.ChatEnergy.Value).padStart(2)}`
-  //       ),
-  //   );
-  // }
+  const commands: ICommand[] = [executeStrategyForStep(environment, state)];
     
   if (state.Tank.ChatEnergy.Value === state.Tank.ChatEnergy.Max) {
     if (Math.random() < 0.02) {
--- strategy.ts
+++ strategy.ts
@@ -7,16 +7,13 @@ import {
   FireGunCommand,
   type IStepCommand,
 } from './commands';
-import type { TankDetails } from './helpers';
 import type { EnvironmentMessage, StatState, StepState, Vector } from './messages';
 
 
 
 export function executeStrategyForStep(
   environment: EnvironmentMessage,
-  state: StepState,
-  // From TankId to TankDetails object
-  allObservedTankScanEvents: Record<number, TankDetails[]>
+  state: StepState
 ): IStepCommand {
   // Dummy strategy:
   return new RotateTurretCommand(10);
